5daa1b564561ca2a7effc2b8f143bd1dbebe39fc
Eurin HASH <eurindelagoldcoast@gmail.com>
2024-05-15 17:30:04 +0100

    Avertissements et sanctions progressives

---
+++ warnings_1.py
@@ -0,0 +1,14 @@
+# warnings.py
+user_warnings = {}
+
+def warn_user(user_id):
+    if user_id not in user_warnings:
+        user_warnings[user_id] = 0
+    user_warnings[user_id] += 1
+    
+    if user_warnings[user_id] == 1:
+        print(f"User {user_id} has been warned for the first time.")
+    elif user_warnings[user_id] == 2:
+        print(f"User {user_id} has been warned for the second time.")
+    else:
+        ban_user(user_id)
