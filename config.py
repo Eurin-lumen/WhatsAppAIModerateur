823f0b23d3c96c5754fc9145fcaf00e4e7591f4d
Eurin HASH <eurindelagoldcoast@gmail.com>
2024-05-15 17:43:54 +0100

    Personnalisation des règles de modération

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
 # config.py
 API_KEY = 'your_whatsapp_api_key'
 GROUP_ID = 'your_whatsapp_group_id'
+SPAM_KEYWORDS = ['spam', 'advertisement', 'promo']
+AD_PATTERNS = [r'http[s]?://', r'\bdiscount\b', r'\bsale\b']
